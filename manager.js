57cf858ab8336c0343cb92fbe55312f9c9718a40
Glitch (hello-express) <none>
2019-02-11 12:00:06 +0000

    👁🛩 Checkpoint ./manager.js:3075273/5361 ./watch.json:3075273/1936

--- manager.js
+++ manager.js
@@ -6,11 +6,18 @@ Manager.spawn(2);
 const http = require('http');
 const express = require('express');
 const app = express();
+
 app.get("/", (request, response) => {
+  response.sendFile(__dirname + '/views/index.html');
   console.log(Date.now() + " Ping Received");
   response.sendStatus(200);
 });
-app.listen(process.env.PORT);
-setInterval(() => {
+
+app.use(express.static('public'))
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`);
+  setInterval(() => {
   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 10000);
+})
