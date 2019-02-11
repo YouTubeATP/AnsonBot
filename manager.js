fc21662fc440ff7cc026dc3181c4d0f4b8f0781d
Glitch (hello-express) <none>
2019-02-11 11:28:06 +0000

    🕴⛲️ Checkpoint ./manager.js:3075273/990 ./package.json:3075273/65 ./watch.json:3075273/26028 ./data/enmap.sqlite-wal:3075273/197612

--- manager.js
+++ manager.js
@@ -2,3 +2,15 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const Manager = new Discord.ShardingManager('./index.js');
 Manager.spawn(2);
+
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 10000);
