8e2293ebe21fd9729c68e1fa545e380a603a39aa
Glitch (musembed) <none>
2019-03-31 03:52:51 +0000

    🏛🍣 Checkpoint ./index.js:3075273/3591 ./watch.json:3075273/1452

--- index.js
+++ index.js
@@ -901,6 +901,32 @@ function play(guild, song){
     }
 }
 
+function pause(guild, song){
+      let fetched = ops.active.get(message.guild.id);
+
+    if (!fetched) {
+        return message.reply("There is no music currently playing");
+    }
+    else if (message.guild.me.voiceChannelID !== message.member.voiceChannelID) {
+        return message.reply("Sorry you are not in the same voice channel as me");
+    }
+    else if (fetched.dispatcher.paused) {
+        return message.reply("The music is already paused!");
+    }
+
+    message.channel.send(`The song has been paused`)
+    await fetched.dispatcher.pause()
+}
+
+exports.config = {
+    aliases: [  ]
+};
+
+exports.help = {
+    name: 'pause'
+}
+}
+
 function sortObject() {
     var arr = [];
     for (var prop in userData) {
