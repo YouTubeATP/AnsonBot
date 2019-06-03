a3e28eb558b8e974743b1cc3173f04930e1732c2
Glitch (musembed) <none>
2019-06-03 11:52:20 +0000

    👬👨‍❤️‍👨 Checkpoint ./manager.js:3075273/2395 ./watch.json:3075273/994 ./debugger.js:3075273/36 ./package.json:3075273/3962

--- manager.js
+++ manager.js
@@ -10,20 +10,7 @@ const app = express();
 const Manager = new Discord.ShardingManager('./index.js');
 Manager.spawn(1);
 
-var proxy = httpProxy.createProxyServer({});
-var server = http.createServer(function(req, res) {
-  proxy.web(req, res, { target: 'http://127.0.0.1:9000', forward: 'http://127.0.0.1:5000' });
-});
-
-console.log(`Listening on port ${process.env.PORT}`)
-server.listen(process.env.PORT);
-
-app.get('/', (req, res) => {
-     console.log(Date.now() + " Ping Received");
-      res.sendStatus(200);
-});
-
-app.listen(9000);
+app.listen(process.env.PORT);
 setInterval(() => {
   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
