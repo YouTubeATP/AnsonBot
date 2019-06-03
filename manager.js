76bdd0bce90d0d598b7174037c4acf6b1aafc5eb
Glitch (musembed) <none>
2019-06-03 15:20:15 +0000

    🎩🎁 Checkpoint ./manager.js:3075273/2 ./watch.json:3075273/2

--- manager.js
+++ manager.js
@@ -35,7 +35,7 @@ dbl.webhook.on('vote', vote => {
   bot.channels.get(`584591025616715786`).send({embed: {
             color: 0x00bdf2,
             title: "Vote Received",
-            description: `@${vote.user} just voted for <@414440610418786314>! Thanks a lot, we really appreciate it.`,
+            description: `<@${vote.user}> just voted for <@414440610418786314>! Thanks a lot, we really appreciate it.`,
             footer: {
                 icon_url: bot.user.avatarURL,
                 text: "MusEmbed™ | Affiliated with Paraborg Discord Bots"
