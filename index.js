bfb9d7cb0fae619323612d0d16f9c62eb8e7313c
Glitch (musembed) <none>
2019-06-07 04:39:32 +0000

    👑💐 Checkpoint ./index.js:3075273/139 ./watch.json:3075273/2

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ bot.on('guildMemberAdd', member => {
   if (member.user.bot) return;
   let memberTag = member.user.id;
   member.addRole(guild.roles.find("name", "Member")).then(() => {
-        bot.channels.get('585811822305738772').send("<@" + memberTag + "> has joined **MusEmbed Support Server**. Welcome, <@" + memberTag + ">.");
+        bot.channels.get('585811822305738772').send("<@" + memberTag + "> has joined **MusEmbed Support**. Welcome, <@" + memberTag + ">.");
       }).catch(e => {
         console.log(e);
       });
@@ -71,7 +71,7 @@ bot.on('guildMemberRemove', member => {
   if(member.user.bot) return;
   let memberTag = member.user.id;
   member.removeRole(guild.roles.find("name", "Member")).then(() => {
-        bot.channels.get('585811822305738772').send("<@" + memberTag + "> has left **MusEmbed Support Server**. Farewell, <@" + memberTag + ">.");
+        bot.channels.get('585811822305738772').send("<@" + memberTag + "> has left **MusEmbed Support**. Farewell, <@" + memberTag + ">.");
       }).catch(e => {
         console.log(e);
       });
@@ -537,7 +537,7 @@ bot.on('message', async message => {
         message.channel.send({embed: {
             color: 0x00bdf2,
             title: "Links",
-            description: ("[MusEmbed's Website](https://www.musembed.tk/) \n[Vote for MusEmbed](https://vote.Musembed.tk/) \n[Join MusEmbed's Support Server](https://invite.gg/paraborg) \n[Invite MusEmbed](https://invite.Musembed.tk/) \n[MusEmbed's Uptime](https://uptime.musembed.tk/) \n[Paraborg Discord Bots](https://paraborg.xyz)"),
+            description: ("[MusEmbed's Website](https://www.musembed.tk/) \n[Vote for MusEmbed](https://vote.Musembed.tk/) \n[Join MusEmbed Support](https://discord.gg/qCame3b) \n[Invite MusEmbed](https://invite.Musembed.tk/) \n[MusEmbed's Uptime](https://uptime.musembed.tk/)"),
             footer: {
                         icon_url: bot.user.avatarURL,
                         text: "MusEmbed™ | Clean Embeds, Crisp Music"
