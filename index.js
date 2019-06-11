c68e80a9049b01d84499d00715692c9723df3684
Glitch (musembed) <none>
2019-06-11 11:32:37 +0000

    📠🍳 Checkpoint ./index.js:3075273/7 ./watch.json:3075273/2

--- index.js
+++ index.js
@@ -585,7 +585,7 @@ bot.on('message', async message => {
         message.channel.send({embed: {
             color: 0x00bdf2,
             title: "Links",
-            description: ("[MusEmbed's Website](https://www.musembed.tk/) \n[Vote for MusEmbed](https://vote.Musembed.tk/) \n[Join MusEmbed Support](https://invite.gg/musembed/) \n[Invite MusEmbed](https://invite.musembed.tk/) \n[MusEmbed's Uptime](https://uptime.musembed.tk/)"),
+            description: ("[MusEmbed's Website](https://www.musembed.tk) \n[Vote for MusEmbed](https://vote.musembed.tk) \n[Join MusEmbed Support](https://invite.gg/musembed) \n[Invite MusEmbed](https://invite.musembed.tk) \n[MusEmbed's Uptime](https://uptime.musembed.tk)"),
             footer: {
                         icon_url: bot.user.avatarURL,
                         text: "MusEmbed™ | Clean Embeds, Crisp Music"
