66e9c0f3a1289467d7b3f42f12f39770f044335c
Glitch (musembed) <none>
2019-06-11 12:16:25 +0000

    🙅🚙 Checkpoint ./index.js:3075273/30163

--- index.js
+++ index.js
@@ -883,6 +883,103 @@ bot.on('message', async message => {
   }};
 
   if (msg.split(" ")[0] === prefix + "mute" || message.isMemberMentioned(bot.user) && msg.includes("mute")) {
+      
+    if (message.isMemberMentioned(bot.user)) {
+      message.delete().catch(O_o=>{});
+      return message.reply (`this command is only available when using this server's prefix, \`${prefix}.\``)
+    }
+      
+    var reason = args.slice(1).join(" ")
+    
+    if (!message.member.hasPermission("MANAGE_MESSAGES") || !message.member.hasPermission("ADMINISTRATOR")) {
+      
+      return message.reply("you don't have sufficient permissions!")
+        .then(message.delete())
+        .catch(console.error)
+        
+    }
+    
+    var mem = message.mentions.members.first();
+    
+    if (!mem) {
+      var embed = new Discord.RichEmbed()
+        .setColor("0x00bdf2")
+        .setTitle("Please mention the user you want me to mute.")
+        .setFooter("MusEmbed | Clean Embeds, Crisp Music", bot.user.avatarURL)
+      
+      return message.channel.send(embed)
+        .then(message.delete())
+        .catch(console.error)
+    }
+    
+    if (mem.hasPermission("MANAGE_MESSAGES") || mem.hasPermission("ADMINISTRATOR")) {
+      
+      var embed = new Discord.RichEmbed()
+        .setColor("0x00bdf2")
+        .setTitle("Thi")
+        .setFooter("MusEmbed | Clean Embeds, Crisp Music", bot.user.avatarURL)
+      
+      return message.channel.send(embed)
+        .then(message.delete())
+        .catch(console.error)
+    }
+    
+    var muteRole = message.guild.roles.find(r => r.name.toLowerCase().startsWith("mute"))
+    
+    function mute(message, mem, muteRole, reason) {
+      mem.addRole(muteRole, reason).then(() => {
+        var embed = new Discord.RichEmbed()
+          .setColor("0x00bdf2")
+          .setAuthor(`${mem.user.tag} has been successfully muted!`, mem.user.avatarURL)
+          .setFooter("MusEmbed | Clean Embeds, Crisp Music", bot.user.avatarURL)
+        
+        if (reason) embed.addField("Reason", reason)
+
+        return message.channel.send(embed)
+          .then(message.delete())
+          .catch(console.error)
+        
+      }).catch(e => {
+        var embed = new Discord.RichEmbed()
+          .setColor("0x00bdf2")
+          .setTitle(`I could not mute ${mem.user.tag}.`)
+          .addField("Error:", e)
+          .setFooter("MusEmbed | Clean Embeds, Crisp Music", bot.user.avatarURL)
+
+        return message.channel.send(embed)
+          .then(message.delete())
+          .catch(console.error)
+      })
+    }
+    
+    if (muteRole && !mem.roles.has(muteRole)) {
+      mute(message, mem, muteRole, reason)
+    } else if (mem.roles.has(muteRole)) {
+      var embed = new Discord.RichEmbed()
+        .setColor("0x00bdf2")
+        .setTitle(`${mem.user.tag} is already muted!`)
+        .setFooter("MusEmbed | Clean Embeds, Crisp Music", bot.user.avatarURL)
+
+      return message.channel.send(embed)
+        .then(message.delete())
+        .catch(console.error)
+    } else {
+      message.guild.createRole({name: 'Muted', color: 0xa8a8a8}, `I was told to mute someone when there is no mute role!`)
+        .then((role) => {
+          var embed = new Discord.RichEmbed()
+            .setColor("0x00bdf2")
+            .setDescription(`Since a mute role was not present, I went ahead and made one for you.`)
+            .addField("Role", `<@&${role.id}>`)
+            .setFooter("MusEmbed | Clean Embeds, Crisp Music", bot.user.avatarURL)
+
+          message.channel.send(embed)
+        
+          mute(message, mem, role, reason)
+        })
+      
+    }}
+  
+  if (msg.split(" ")[0] === prefix + "mute" || message.isMemberMentioned(bot.user) && msg.includes("mute")) {
     if (!message.member.hasPermission("MANAGE_MESSAGES") || !message.member.hasPermission("ADMINISTRATOR")) {
         message.delete().catch(O_o=>{});
         message.reply("you don't have sufficient permissions!");
@@ -923,34 +1020,6 @@ bot.on('message', async message => {
         console.log(e);
       });
            }};
-
-  if (msg.split(" ")[0] === prefix + "unmute" || message.isMemberMentioned(bot.user) && msg.includes("unmute")) {
-    if (!message.member.hasPermission("MANAGE_MESSAGES") || !message.member.hasPermission("ADMINISTRATOR")) {
-        message.delete().catch(O_o=>{});
-        return message.reply("you don't have sufficient permissions!");
-    }
-    if (message.isMemberMentioned(bot.user)) {
-      message.delete().catch(O_o=>{});
-      return message.reply (`this command is only available when using this server's prefix, \`${prefix}.\``)
-    } else {var mem = message.mentions.members.first();
-    if (mem.roles.some(role => role.name === 'Muted')) {
-        message.delete().catch(O_o=>{});
-        mem.removeRole(message.guild.roles.find("name", "Muted")).then(() => {
-        return message.channel.send(mem.displayName + " has successfully been unmuted!")
-      }).catch(e => {
-        message.delete().catch(O_o=>{});
-        return message.channel.send({embed: {
-            color: 0x00bdf2,
-            description:("An error occured!"),
-            footer: {
-                icon_url: bot.user.avatarURL,
-                text: "MusEmbed™ | Clean Embeds, Crisp Music"
-            }
-  }});
-        console.log(e);
-      })}
-        message.delete().catch(O_o=>{});
-        return message.reply("this user was not muted.")}};
   
 });
 
