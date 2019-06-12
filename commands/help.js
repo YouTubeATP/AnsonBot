b5fcc096b1ee9759910e92b65e467552c1eea3dc
Glitch (musembed) <none>
2019-06-12 07:11:59 +0000

    🌷✨ Checkpoint ./commands/help.js:4959043/6043

--- commands/help.js
+++ commands/help.js
@@ -8,6 +8,8 @@ module.exports = {
 	description: "Get help information!",
 	run: async (bot, message, args, shared) => {
     
+    const commands = bot.commands
+    
     if (args.length == 0) {
       
       var embed = new Discord.RichEmbed()
@@ -17,55 +19,36 @@ module.exports = {
         .addField("Description", "This is a bot that can help you easily create embeds in your server. We also have music playing functionality.")
         .addField("General Commands", "`help`: Shows this help message.\n`botinfo`: Retrieves information about the bot.\n`serverinfo`: Retrieves information about the server.\n`ping`: Shows latency of the bot and the API.\n`links`: Shows all our links.\n`suggestion`: Submits a suggestion to MusEmbed's support server.")
         .addField("Embed Commands", "`embed`: Embeds your message.\n`rawembed`: Embeds your message without showing your name.")
-      .addField("Music Commands", "`play`: Searches for the song you requested.\n`pause`: Pauses the current song.\n`resume`: Resumes a paused song.\n`skip`: Votes to skip the playing song. Song automatically skips if half/over half of people vote to skip.\n
-np: Shows the name of the currently playing song. 
-volume ([number]): Changes volume to the provided number. Shows current volume if arguments are not provided. 
-queue: Shows the current queue of songs. 
-loop: Toggles loop to single when used once. Toggles to all when used twice. Toggles off when used thrice. 
-stop: Moderator-only command. Resets the queue and stops music. Also forces bot to leave the voice channel.")
+        .addField("Music Commands", "`play`: Searches for the song you requested.\n`pause`: Pauses the current song.\n`resume`: Resumes a paused song.\n`skip`: Votes to skip the playing song.\n`np`: Shows the name of the currently playing song.\n`volume`: Shows or changes volume to the provided number.\n`queue`: Shows the current queue of songs.\n`loop`: Toggles loop.\n`stop`: Stops all music.")
+        .addField("Moderation Commands", "`kick`: Kicks a user from the guild.\n`ban`: Bans a user from the guild.\n`purge`: Deletes a number of messages in a channel.\n`mute`: Mutes a user in the guild.\n`unmute`: Unmutes a user in the guild.")
+        .addField("Server Configuration Commands", "`showconf`: Shows the configurations of the server.\n`setconf`: Sets new value for the server's configuration.")
+        .addField("\u200B", `You can do \`${shared.prefix}help [command]\` to get information about specific commands!`)
+        .setFooter("MusEmbed | Clean Embeds, Crisp Music", bot.user.avatarURL)
       
+      message.channel.send(embed)
+        .then(message.delete())
+        .catch(console.error)
       
+    } else if (args.length == 1) {
       
-      message.delete().catch(O_o=>{});
-        message.channel.send({embed: {
-            color: 0x00bdf2,
-            title: "MusEmbed's Help Message",
-            fields: [
-                    {
-                        name: "Prefix",
-                        value: ("The prefix for this server is \`" + shared.prefix + "\`. You may also mention me as a prefix.\n")
-                    },
-                    {
-                        name: "Description",
-                        value: "This is a bot that can help you easily create embeds in your server. We also have music playing functionality. \n"
-                    },
-                    {
-                        name: "General Commands",
-                        value: "\`help\`: Shows this help message. \n\`botinfo\`: Retrieves information about the bot. \n\`serverinfo\`: Retrieves information about the server. \n\`ping\`: Shows latency of the bot and the API. \n\`links\`: Shows all our links. \n\`suggest [suggestion]\`: Submits a suggestion to MusEmbed's support server. Suggestion must consist of 20 characters or more. \n"
-                    },
-                    {
-                        name: "Embed Commands",
-                        value: "\`embed [(hex code]) [message]\`: Embeds your message. \n\`rawembed ([hex code]) [message]\`: Administrator-only command that embeds your message without your name. \n"
-                    },
-                    {
-                        name: "Music Commands",
-                        value: "\`play [name of music]\`: Searches for the song you requested. \n\`pause\`: Pauses the current song. \n\`resume\`: Resumes a paused song. \n\`skip\`: Votes to skip the playing song. Song automatically skips if half/over half of people vote to skip. \n\`np\`: Shows the name of the currently playing song. \n\`volume ([number])\`: Changes volume to the provided number. Shows current volume if arguments are not provided. \n\`queue\`: Shows the current queue of songs. \n\`loop\`: Toggles loop to \`single\` when used once. Toggles to \`all\` when used twice. Toggles \`off\` when used thrice. \n\`stop\`: Moderator-only command. Resets the queue and stops music. Also forces bot to leave the voice channel."
-                    },
-                    {
-                        name: "Moderation Commands",
-                        value: "\`kick [user]\`: Kicks a user from the guild. \n\`ban [user]\`: Bans a user from the guild. \n\`purge [number]\`: Deletes a number of messages in a channel. \n\`mute [user]\`: Mutes a user in the guild. A 'Muted' role must first be set up for this to work. \n\`unmute [user]\`: Unmutes a user in the guild. A 'Muted' role must first be set up for this to work. \n"
-                    },
-                    {
-                        name: "Server Configurations",
-                        value: "\`showconf\`: Shows the configurations for your server. \n\`setconf [item] [new value]\`: Administrator-only command. Sets a new value for your server's configuration. For this command, \`em/\` is a permanent prefix. \n(Available configurations: \`prefix (new prefix)\`, \`censor (on/off)\`) \n" 
-                    },
-                ],
-        footer: {
-                    icon_url: bot.user.avatarURL,
-                    text: "MusEmbed™ | Clean Embeds, Crisp Music"
-                }
-  }})
-}
+      const name = args[0].toLowerCase();
+			const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+
+			if (!command) {
+				return message.reply('that\'s not a valid command!');
+			}
+
+			var embed = new Discord.RichEmbed().setColor(0x42b3f4).setAuthor(`${shared.prefix}${command.name}`, bot.user.avatarURL)
+
+			if (command.aliases) embed.addField(`Aliases`, "`" + command.aliases.join('`, `') + "`")
+			if (command.description) embed.addField(`Description`, command.description)
+			if (command.usage) embed.addField(`Usage`,`\`${command.usage}\``)
+
+			message.channel.send(embed)
+      
+    } else {
+      shared.printError(message, "Too many arguments", "You inputted too many arguments!")
+    }
     
     
 	}
