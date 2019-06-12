fba697ba1eccfd2003e5fd6ccd21176f8db11390
Glitch (musembed) <none>
2019-06-12 06:47:52 +0000

    ⚡️🍤 Checkpoint ./commands/help.js:4959043/1991

---
+++ commands/help.js
@@ -0,0 +1,14 @@
+const index = require('../index.js');
+const fs = require('fs');
+const Discord = require('discord.js');
+
+module.exports = {
+	name: "help",
+	usage: "help [command]",
+	description: "Get help information!",
+	run: async (bot, message, args, shared) => {
+    
+    if (args.length == 0)
+    
+	}
+}
