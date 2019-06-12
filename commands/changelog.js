00eb88e0f06b44db675797bf9387186deb9cad2d
Glitch (musembed) <none>
2019-06-12 11:52:54 +0000

    💂🎃 Checkpoint ./commands/changelog.js:4959043/1212

--- commands/changelog.js
+++ commands/changelog.js
@@ -12,30 +12,11 @@ module.exports = {
     
     if (message.author.id != "336389636878368770" && message.author.id != "344335337889464357") return;
 
-		try {
-			var out = eval(args.join(' '))
-			// out = JSON.stringify(out)
-      
-      var embed = new Discord.RichEmbed()
-        .setColor("GREEN")
-        .setTitle(`<:green_tick:588269976658378768> Evaluation Success!`)
-        .addField(`Expression`, '```js\n'+args.join(" ")+'```')
-        .addField(`Result`, '```js\n'+out+'```')
-        .setFooter("MusEmbed™ | Clean Embeds, Crisp Music", bot.user.avatarURL)
-			message.channel.send(embed)
-        .then(message.delete())
-        .catch(console.error)
-		} catch (e) {
-      var embed = new Discord.RichEmbed()
-        .setColor("RED")
-        .setTitle(`<:red_tick:588269975798808588> Evaluation Failed!`)
-        .addField(`Expression`, '```js\n'+args.join(" ")+'```')
-        .addField(`Error Message`, '```js\n'+e+'```')
-        .setFooter("MusEmbed™ | Clean Embeds, Crisp Music", bot.user.avatarURL)
-			message.channel.send(embed)
-        .then(message.delete())
-        .catch(console.error)
-		}
+		var version = args[0]
+    var content = message.content.slice(shared.prefix.length + 10 + args[0].length).trim()
+    
+    var embed = new Discord.RichEmbed()
+      .setTitle(`Cha
     
 	}
 }
