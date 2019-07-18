576c6a576e5134169579c78e4b9d1431067a1beb
Glitch (musembed) <none>
2019-07-18 16:17:12 +0000

    🍓👂 Checkpoint ./commands/pause.js:3075273/1117

--- commands/pause.js
+++ commands/pause.js
@@ -2,13 +2,35 @@ const index = require('../index.js');
 const fs = require('fs');
 const Discord = require('discord.js');
 
+const queue = new Map();
+
 module.exports = {
 	name: "pause",
 	usage: "pause",
 	description: "Pauses the current song.",
 	run: async (bot, message, args, shared) => {
     
-    return;
+    const serverQueue = queue.get(message.guild.id);
+    
+    const guildConf = bot.settings.ensure(message.guild.id, defaultSettings)
+    
+    message.delete().catch(O_o=>{});
+      
+      const voiceChannel = message.member.voiceChannel;
+      const botVoiceConnection = message.guild.voiceConnection;
+        
+      if (!voiceChannel) return message.channel.send('You need to be in a voice channel to execute this command!')
+      
+      if (!serverQueue) return message.channel.send("Nothing is playing right now!")
+      
+      if (voiceChannel !== botVoiceConnection.channel) return message.channel.send('You need to be in my voice channel to execute this command!')
+      
+		  if (serverQueue && serverQueue.playing) {
+			serverQueue.playing = false;
+			serverQueue.connection.dispatcher.pause();
+			return message.channel.send('Music paused. Use the command \`' + prefix + 'resume\` to resume playing.');
+		}
+		return message.channel.send('Nothing is playing!');
     
 	}
 }
