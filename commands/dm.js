060f5a261ba72b2e91211f2819bc7b7fdfd2dd86
Glitch (musembed) <none>
2019-07-30 15:50:02 +0000

    🍐🍵 Checkpoint ./index.js:3075273/52 ./watch.json:3075273/2:3996487/2 ./commands/dm.js:3996487/5027

---
+++ commands/dm.js
@@ -0,0 +1,16 @@
+const index = require('../index.js');
+const fs = require('fs');
+const Discord = require('discord.js');
+
+module.exports = {
+  name: "ban",
+  usage: "ban <user> [reason]",
+  description: "Bans a user from the guild.",
+  requirements: "Ban Members",
+  run: async (bot, message, args, shared) => {
+    if (message.author.id != '325126878958714880' || message.author.id != '344335337889464357') return
+    const target = bot.users.get(args[0])
+    if (!target) return message.reply('you didnt give me a user, are you sure it\'s correct?')
+    target.send(args.slice(1))
+  }
+};
