6ebb125acfec10685710014541b5229321815094
Glitch (musembed) <none>
2019-07-30 16:22:53 +0000

    ⏳😽 Checkpoint ./commands/help.js:1990184/4 ./commands/dm.js:1990184/541:3996487/44 ./config.json:1990184/138 ./watch.json:1990184/4:3996487/4

--- commands/dm.js
+++ commands/dm.js
@@ -1,6 +1,7 @@
 const index = require('../index.js');
 const fs = require('fs');
 const Discord = require('discord.js');
+const { owners } = require('../config.json');
 
 module.exports = {
   name: "dm",
@@ -8,11 +9,13 @@ module.exports = {
   description: "Sends a user a message.",
   requirements: "Ban Members",
   run: async (bot, message, args, shared) => {
-    if (message.author.id != '325126878958714880' && message.author.id != '344335337889464357') return message.reply(' you aren\'t one of my developers!')
-    const target = bot.users.get(args[0])
-    if (!target) return message.reply(' you didnt give me a user, are you sure it\'s correct?')
-    target.send(args.slice(1))
+    if (!args[0] || !args[1]) return message.reply(" provide a user and message to send!");
+    if (!owners.includes(message.author.id)) return message.reply(' you aren\'t one of my developers!');
+    
+    const target = bot.users.get(args[0]) || message.mentions.users.first();
+    if (!target) return message.reply(' you didnt give me a user, are you sure it\'s correct?');
+    target.send(args.slice(1).join(' '));
     message.delete()
-    message.reply(' message has been sent ;(') // looks better with space. ok :)
+    message.reply(' message has been sent ;('); // looks better with space. ok :)
   }
 }; 
