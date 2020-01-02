2a1d69794ccad84c1ed1f629591db4f59630df77
Glitch (musembed) <none>
2020-01-02 11:35:12 +0000

    🎯🐝 Checkpoint ./util/logging/guildMemberRemove.js:3075273/5155 ./util/logging/guildMemberUpdate.js:3075273/10114 ./util/logging/guildUpdate.js:3075273/10691 ./util/logging/messageDelete.js:3075273/2775 ./util/logging/messageDeleteBulk.js:3075273/2462 ./util/logging/channelUpdate.js:3075273/11230 ./util/logging/channelCreate.js:3075273/4318 ./util/logging/channelDelete.js:3075273/4270 ./util/logging/messageUpdate.js:3075273/3086 ./util/logging/roleCreate.js:3075273/2530 ./util/logging/roleDelete.js:3075273/2530 ./watch.json:3075273/2 ./util/logging/roleUpdate.js:3075273/14170 ./util/logging/voiceStateUpdate.js:3075273/7736 ./util/config.js:3075273/563 ./util/fn.js:3075273/15057 ./util/logging.js:3075273/1270 ./util/tempmute.js:3075273/5902 ./package.json:3075273/8960

---
+++ util/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  defaultPrefix: "m/",
+  embedColor: 0x00bdf2,
+  support: "502482252756680725",
+  owner: "344335337889464357",
+  permissions: 2146958847
+};
---
+++ util/logging/roleUpdate.js
@@ -0,0 +1,64 @@
+const Discord = require("discord.js"),
+  db = require("quick.db");
+
+const guildData = new db.table("GUILDDATA");
+
+const config = require("/app/util/config"),
+  fn = require("/app/util/fn");
+
+module.exports = client => {
+  client.on("roleUpdate", async (oldRole, newRole) => {
+    let logChannelID = guildData.get(`${newRole.id}.botlog`);
+    let logChannel = client.channels.get(logChannelID);
+    if (!logChannel) return;
+
+    if (oldRole.name != newRole.name)
+      return logChannel.send(
+        new Discord.RichEmbed()
+          .setColor(config.embedColor)
+          .setAuthor("Role Name Updated")
+          .setThumbnail(newRole.guild.iconURL)
+          .addField("Role", `${newRole} (${newRole.name})`)
+          .addField("Before", `${oldRole.name}`, true)
+          .addField("After", `${newRole.name}`, true)
+          .setFooter(client.user.username, client.user.avatarURL)
+          .setTimestamp()
+      );
+
+    if (oldRole.color != newRole.color)
+      return logChannel.send(
+        new Discord.RichEmbed()
+          .setColor(config.embedColor)
+          .setAuthor("Role Color Updated")
+          .setThumbnail(newRole.guild.iconURL)
+          .addField("Role", `${newRole} (${newRole.name})`)
+          .addField("Before", `${oldRole.hexColor}`, true)
+          .addField("After", `${newRole.hexColor}`, true)
+          .setFooter(client.user.username, client.user.avatarURL)
+          .setTimestamp()
+      );
+
+    if (oldRole.mentionable != newRole.mentionable) {
+      if (newRole.mentionable)
+        return logChannel.send(
+          new Discord.RichEmbed()
+            .setColor(config.embedColor)
+            .setAuthor("Role Mentionable")
+            .setThumbnail(newRole.guild.iconURL)
+            .addField("Role", `${newRole} (${newRole.name})`)
+            .setFooter(client.user.username, client.user.avatarURL)
+            .setTimestamp()
+        );
+      else
+        return logChannel.send(
+          new Discord.RichEmbed()
+            .setColor(config.embedColor)
+            .setAuthor("Role Unmentionable")
+            .setThumbnail(newRole.guild.iconURL)
+            .addField("Role", `${newRole} (${newRole.name})`)
+            .setFooter(client.user.username, client.user.avatarURL)
+            .setTimestamp()
+        );
+    }
+  });
+};
