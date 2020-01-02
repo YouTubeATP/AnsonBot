fd5c0695ea46f801b986a28148194dd3b29ce845
Glitch (musembed) <none>
2020-01-02 12:35:12 +0000

    🍦🌏 Checkpoint ./commands/case.js:3075273/11868 ./commands/config.js:3075273/180520 ./commands/echo.js:3075273/3030 ./commands/eval.js:3075273/8223 ./commands/giverole.js:3075273/6916 ./commands/guildinfo.js:3075273/9177 ./README.md:3075273/875 ./watch.json:3075273/6

---
+++ commands/eval.js
@@ -0,0 +1,97 @@
+const Discord = require("discord.js"),
+  db = require("quick.db"),
+  moment = require("moment");
+
+const userData = new db.table("USERDATA"),
+  guildData = new db.table("GUILDDATA"),
+  modCases = new db.table("MODCASES"),
+  botData = new db.table("BOTDATA");
+
+const config = require("/app/util/config"),
+  fn = require("/app/util/fn");
+
+module.exports = {
+  name: "eval",
+  usage: "eval <code>",
+  description: "Evaluates JavaScript code.",
+  category: "Bot Staff",
+  botStaffOnly: true,
+  run: async (client, message, args, shared) => {
+    const msg = message,
+      bot = client;
+
+    let modifier = "-e";
+
+    if (
+      args[args.length - 1] == "-t" ||
+      args[args.length - 1] == "-l" ||
+      args[args.length - 1] == "-e"
+    ) {
+      modifier = args.pop();
+    }
+
+    try {
+      var out = eval(args.join(" "));
+      out = JSON.stringify(out);
+
+      if (modifier == "-e" && out.length <= 1024 - 8)
+        message.channel
+          .send(
+            new Discord.RichEmbed()
+              .setColor("GREEN")
+              .setTitle(
+                `${client.guilds
+                  .get(config.support)
+                  .emojis.find(
+                    emoji => emoji.name == "green_tick"
+                  )} Evaluation Success!`
+              )
+              .addField(`Expression`, "```js\n" + args.join(" ") + "```")
+              .addField(`Result`, "```js\n" + out + "```")
+              .setFooter(client.user.username, client.user.avatarURL)
+          )
+          .catch(console.error);
+      else if (
+        out.length <= 2000 - 8 &&
+        (modifier == "-t" || (modifier == "-e" && out.length > 1024 - 8))
+      )
+        message.channel.send("```js\n" + out + "```");
+      else if ((modifier = "-l"))
+        console.log(`${fn.time()} | Evaluation Result | ${out}`);
+      else {
+        console.log(`${fn.time()} | Evaluation Result | ${out}`);
+        message.channel
+          .send(
+            new Discord.RichEmbed()
+              .setColor("GREEN")
+              .setTitle(
+                `${client.guilds
+                  .get(config.support)
+                  .emojis.find(
+                    emoji => emoji.name == "green_tick"
+                  )} Evaluation Success!`
+              )
+              .addField(`Expression`, "```js\n" + args.join(" ") + "```")
+              .addField(
+                `Result`,
+                "```js\nOutput too long. Check console log.```"
+              )
+              .setFooter(client.user.username, client.user.avatarURL)
+          )
+          .catch(console.error);
+      }
+    } catch (e) {
+      var embed = new Discord.RichEmbed()
+        .setColor("RED")
+        .setTitle(
+          `${client.guilds
+            .get(config.support)
+            .emojis.find(emoji => emoji.name == "red_tick")} Evaluation Failed!`
+        )
+        .addField(`Expression`, "```js\n" + args.join(" ") + "```")
+        .addField(`Error Message`, "```js\n" + e + "```")
+        .setFooter(client.user.username, client.user.avatarURL);
+      message.channel.send(embed).catch(console.error);
+    }
+  }
+};
