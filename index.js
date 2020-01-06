ddcc05d23317c53a1415bc35d5274598e2973786
Glitch (musical-pal) <none>
2020-01-06 11:37:54 +0000

    💆🏨 Checkpoint ./index.js:3075273/110021 ./watch.json:3075273/8

--- index.js
+++ index.js
@@ -158,11 +158,15 @@ client.on("message", async message => {
   if (
     (message.channel.id === "663694873227952128" &&
       message.guild.id === config.server &&
-      (message.author.bot &&
-        message.author.id !== client.user.id &&
-        message.author.id !== "365975655608745985")) ||
-    (!message.author.bot &&
-      !message.content.toLowerCase().includes("p!catch" | "p!hint"))
+      message.author.bot &&
+      message.author.id !== client.user.id &&
+      message.author.id !== "365975655608745985") ||
+    (message.channel.id === "663694873227952128" &&
+      message.guild.id === config.server &&
+      !message.author.bot &&
+      message.author.id !== client.user.id &&
+      !message.content.toLowerCase().includes("p!catch") &&
+      !message.content.toLowerCase().includes("p!hint"))
   )
     return message.delete();
 
