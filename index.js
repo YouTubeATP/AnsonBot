8c5978666b87c7e8ac36b446701e746b3590d820
Glitch (musembed) <none>
2020-01-06 07:17:19 +0000

    👼👨‍👨‍👦 Checkpoint ./index.js:3075273/6 ./watch.json:3075273/2

--- index.js
+++ index.js
@@ -398,7 +398,7 @@ client.on("voiceStateUpdate", (oldMember, newMember) => {
           oldMember.voiceChannel.members.size <= 0
         ) {
           oldMember.voiceChannel.delete("Served its purpose");
-          for (j = i + 1; j <= i; j++) {
+          for (j = 1; j <= i; j++) {
             try {
               if (
                 guild.channels.find("name", `Public Lounge #${i}`) &&
