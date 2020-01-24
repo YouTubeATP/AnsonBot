a066ab3589a124a554cbb5f0e9d6335b4fa4281c
Glitch (musical-pal) <none>
2020-01-24 15:24:14 +0000

    🌲🎢 Checkpoint ./index.js:3075273/18 ./watch.json:3075273/4

--- index.js
+++ index.js
@@ -35,17 +35,17 @@ const listener = app.listen(process.env.PORT, function() {
 });
 
 const antiSpam = new AntiSpam({
-  warnThreshold: 5, // Amount of messages sent in a row that will cause a warning.
-  kickThreshold: 7, // Amount of messages sent in a row that will cause a ban.
-  banThreshold: 12, // Amount of messages sent in a row that will cause a ban.
-  maxInterval: 1000, // Amount of time (in milliseconds) in which messages are considered spam.
+  warnThreshold: 3, // Amount of messages sent in a row that will cause a warning.
+  kickThreshold: 5, // Amount of messages sent in a row that will cause a ban.
+  banThreshold: 7, // Amount of messages sent in a row that will cause a ban.
+  maxInterval: 1500, // Amount of time (in milliseconds) in which messages are considered spam.
   warnMessage:
     "{@user}, please refrain from spamming the chat. Further infractions will result in a kick or ban.", // Message that will be sent in chat upon warning a user.
   kickMessage: "**{user_tag}** has been kicked for spamming.", // Message that will be sent in chat upon kicking a user.
   banMessage: "**{user_tag}** has been banned for spamming.", // Message that will be sent in chat upon banning a user.
-  maxDuplicatesWarning: 5, // Amount of duplicate messages that trigger a warning.
-  maxDuplicatesKick: 7, // Amount of duplicate messages that trigger a warning.
-  maxDuplicatesBan: 12, // Amount of duplicate messages that trigger a warning.
+  maxDuplicatesWarning: 3, // Amount of duplicate messages that trigger a warning.
+  maxDuplicatesKick: 5, // Amount of duplicate messages that trigger a warning.
+  maxDuplicatesBan: 7, // Amount of duplicate messages that trigger a warning.
   exemptPermissions: ["ADMINISTRATOR"], // Bypass users with any of these permissions.
   ignoreBots: true, // Ignore bot messages.
   verbose: true, // Extended Logs from module.
