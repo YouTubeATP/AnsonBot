971513e4f1004500a6795ab455e6bf611d1452c5
Glitch (musical-pal) <none>
2020-02-18 18:55:47 +0000

    🚃😇 Checkpoint ./commands/ping.js:3075273/13930 ./commands/play.js:3075273/309 ./watch.json:3075273/2

---
+++ commands/ping.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+
+const config = require("/app/util/config"),
+  fn = require("/app/util/fn");
+
+module.exports = {
+  name: "ping",
+  usage: "ping",
+  aliases: ["pong"],
+  description: "Pings the bot and shows its latency.",
+  category: "Utility",
+  run: async (client, message, args, shared) => {
+    message.channel.send("Pinging...").then(m => {
+      let pingMessage = `Latency: \`${Math.round(client.ping)}\`ms.`;
+
+      let embed = new Discord.RichEmbed()
+        .setColor("GREEN")
+        .setAuthor(message.author.tag, message.author.avatarURL)
+        .setThumbnail(client.user.avatarURL)
+        .setTitle("Ping Received!")
+        .setDescription(pingMessage)
+        .setFooter(client.user.username, client.user.avatarURL)
+        .setTimestamp();
+
+      message.channel
+        .send(embed)
+        .then(() => {
+          message.delete();
+          m.delete();
+        })
+        .catch(console.error);
+    });
+  }
+};
---
+++ commands/play.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+
+const config = require("/app/util/config"),
+  fn = require("/app/util/fn");
+
+module.exports = {
+  name: "help",
+  usage: "help [command]",
+  description: "Shows command information.",
+  category: "Utility",
+  run: async (client, message, args, shared) => {
+    
+  }
+};
