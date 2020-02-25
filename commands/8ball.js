2e1ebfc2de041e918e1f172e3fe1a3f2fdccc5cd
Glitch (musical-pal) <none>
2020-02-25 09:17:12 +0000

    🎪💂 Checkpoint ./.glitch-assets:3075273/394 ./commands/8ball.js:3075273/546 ./watch.json:3075273/2 ./util/8ball.json:3075273/1285

--- commands/8ball.js
+++ commands/8ball.js
@@ -7,7 +7,7 @@ const config = require("/app/util/config"),
 module.exports = {
   name: "8ball",
   usage: "8ball <question>",
-  description: "Gives you a random fortune.",
+  description: "Have the trusty 8-ball answer all your questions!",
   category: "Misc",
   run: async (client, message, args, shared) => {
     let question = message.content.slice(shared.prefix.length + 5).trim();
@@ -15,20 +15,21 @@ module.exports = {
       let embed = new Discord.RichEmbed()
         .setColor("RED")
         .setAuthor(message.author.tag, message.author.avatarURL)
-        .setThumbnail(client.user.displayAvatarURL)
-        .setTitle(`No question provided! | :8ball:`)
+        .setThumbnail("https://cdn.glitch.com/018cc8ba-350d-4bd4-847b-a54addad6e97%2F33571_preview.png?v=1582621594066")
+        .setTitle(`No question provided!`)
         .setDescription("Provide a question and try again.")
         .setFooter(client.user.username, client.user.avatarURL)
         .setTimestamp();
 
       return message.channel.send(embed);
     } else {
+      let answer = eightball;
       let embed = new Discord.RichEmbed()
         .setColor(config.embedColor)
         .setAuthor(message.author.tag, message.author.avatarURL)
-        .setThumbnail(client.user.displayAvatarURL)
-        .setTitle(`${question} | :8ball:`)
-        .setDescription(eightball)
+        .setThumbnail("https://cdn.glitch.com/018cc8ba-350d-4bd4-847b-a54addad6e97%2F33571_preview.png?v=1582621594066")
+        .setTitle(`${question}`)
+        .setDescription(`${answer}`)
         .setFooter(client.user.username, client.user.avatarURL)
         .setTimestamp();
 
