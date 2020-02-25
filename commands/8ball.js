8321f3db3ffacfbd03c479da7a1e04a934df358b
Glitch (musical-pal) <none>
2020-02-25 09:05:45 +0000

    🎤🍳 Checkpoint ./commands/8ball.js:3075273/5972 ./watch.json:3075273/2 ./commands/fortune.js:3075273/1656

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,24 +1,38 @@
 const Discord = require("discord.js"),
-      eightball = require("8ball");
+  eightball = require("8ball");
 
 const config = require("/app/util/config"),
   fn = require("/app/util/fn");
 
 module.exports = {
   name: "8ball",
-  usage: "8ball",
-  aliases: ["8-ball", "eightball", "eight-ball"],
+  usage: "8ball <question>",
   description: "Gives you a random fortune.",
   category: "Misc",
   run: async (client, message, args, shared) => {
-    let fortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    let embed = new Discord.RichEmbed()
+    let question = message.content.slice(shared.prefix.length + 5).trim();
+    if (!question) {
+      let embed = new Discord.RichEmbed()
+        .setColor("RED")
+        .setAuthor(message.author.tag, message.author.avatarURL)
+        .setThumbnail(client.user.displayAvatarURL)
+        .setTitle(`No question provided! | :8ball:`)
+        .setDescription("Provide a question and try again.")
+        .setFooter(client.user.username, client.user.avatarURL)
+        .setTimestamp();
+
+      return message.channel.send(embed);
+    } else {
+      let embed = new Discord.RichEmbed()
         .setColor(config.embedColor)
         .setAuthor(message.author.tag, message.author.avatarURL)
         .setThumbnail(client.user.displayAvatarURL)
-        .setDescription(fortune)
-        .setFooter(client.user.username, client.user.avatarURL);
+        .setTitle(`${question} | :8ball:`)
+        .setDescription(eightball)
+        .setFooter(client.user.username, client.user.avatarURL)
+        .setTimestamp();
 
       return message.channel.send(embed);
+    }
   }
 };
--- commands/fortune.js
+++ commands/fortune.js
@@ -13,12 +13,12 @@ module.exports = {
   run: async (client, message, args, shared) => {
     let fortune = fortunes[Math.floor(Math.random() * fortunes.length)];
     let embed = new Discord.RichEmbed()
-        .setColor(config.embedColor)
-        .setAuthor(message.author.tag, message.author.avatarURL)
-        .setThumbnail(client.user.displayAvatarURL)
-        .setDescription(fortune)
-        .setFooter(client.user.username, client.user.avatarURL);
+      .setColor(config.embedColor)
+      .setAuthor(message.author.tag, message.author.avatarURL)
+      .setDescription(fortune)
+      .setFooter(client.user.username, client.user.avatarURL)
+      .setTimestamp();
 
-      return message.channel.send(embed);
+    return message.channel.send(embed);
   }
 };
