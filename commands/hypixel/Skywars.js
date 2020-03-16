54382cbdb3af38c59e15400c07467afac2e96f5f
Glitch (musical-pal) <none>
2020-03-16 16:46:46 +0000

    🐪🚊 Checkpoint ./commands/hypixel/Skywars.js:3075273/949

--- commands/hypixel/Skywars.js
+++ commands/hypixel/Skywars.js
@@ -18,6 +18,25 @@ let hypixel,
   hypixel1 = new Hypixel({ key: process.env.HYAPI1 }),
   hypixel2 = new Hypixel({ key: process.env.HYAPI2 });
 
+const modes = {
+  solo_normal: "Solo (Normal)",
+  solo_insane: "Solo (Insane)",
+  teams_normal: "Doubles (Normal)",
+  teams_insane: "Doubles (Insane)",
+  mega_doubles: "Mega",
+  eight_two_rush: "Doubles Rush",
+  four_four_rush: "4v4v4v4 Rush",
+  eight_two_ultimate: "Doubles Ultimate",
+  four_four_ultimate: "4v4v4v4 Ultimate",
+  eight_two_voidless: "Doubles Voidless",
+  four_four_voidless: "4v4v4v4 Voidless",
+  eight_two_armed: "Doubles Armed",
+  four_four_armed: "4v4v4v4 Armed",
+  eight_two_lucky: "Doubles Lucky Blocks",
+  four_four_lucky: "4v4v4v4 Lucky Blocks",
+  castle: "Castle"
+};
+
 module.exports = {
   name: "Skywars",
   run: async (
